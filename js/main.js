ed0ce3413bd6cf29974b447eea162379670cb99f
Rodolfo <brsrkdev@gmail.com>
2019-12-03 15:48:10 -0600

    Fix linter errors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-let form = document.getElementById("form");
+const form = document.getElementById('form');
 
-let myLibrary = [];
-let testLibrary = JSON.parse(localStorage.getItem("testLibrary"));
+const myLibrary = [];
+let testLibrary = JSON.parse(localStorage.getItem('testLibrary'));
 
 if (testLibrary == null) {
-  localStorage.setItem("testLibrary", JSON.stringify(myLibrary));
+  localStorage.setItem('testLibrary', JSON.stringify(myLibrary));
 }
+
+function checkRead(book, i) {
+  if (book.read) {
+    return `<button class="btn read" id='read-btn${i}'>Already read</button>`;
+  }
+  return `<button class="btn not-read" id='read-btn${i}'>You haven't read this book</button>`;
+}
+
+function render() {
+  const container = document.getElementById('book-cont');
+  container.innerHTML = '';
+  testLibrary = JSON.parse(localStorage.getItem('testLibrary'));
+  testLibrary.forEach((book, i) => {
+    container.innerHTML += `<div class="book">
+                            <h1>${book.title}</h1>
+                            <h2>${book.author}</h2>
+                            <h2>${book.pages}</h2>
+                            <h2><span>Status:</span>${checkRead(book, i)}</h2>
+                            <button class="btn delete" id='delete-book${i}'>Delete</button>
+                            </div>`;
+
+    document.getElementById(`delete-book${i}`).addEventListener('click', removeBook(i), true);
+  });
+}
+
 render();
 
 function Book(title, author, pages, read) {
@@ -16,71 +41,52 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  formData = {
-    title: document.getElementById("title").value,
-    author: document.getElementById("author").value,
-    pages: document.getElementById("pages").value,
-    read: document.getElementById("already-read").checked
+  const formData = {
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value,
+    pages: document.getElementById('pages').value,
+    read: document.getElementById('already-read').checked,
   };
   const book = new Book(
     formData.title,
     formData.author,
     formData.pages,
-    formData.read
+    formData.read,
   );
   myLibrary.push(book);
-  let testLibrary = JSON.parse(localStorage.getItem("testLibrary"));
+  testLibrary = JSON.parse(localStorage.getItem('testLibrary'));
   testLibrary.push(book);
-  localStorage.setItem("testLibrary", JSON.stringify(testLibrary));
+  localStorage.setItem('testLibrary', JSON.stringify(testLibrary));
   render();
 }
 
 function removeBook(i) {
-  let testLibrary = JSON.parse(localStorage.getItem("testLibrary"));
+  testLibrary = JSON.parse(localStorage.getItem('testLibrary'));
   testLibrary.splice(i, 1);
-  localStorage.setItem("testLibrary", JSON.stringify(testLibrary));
+  localStorage.setItem('testLibrary', JSON.stringify(testLibrary));
   myLibrary.splice(i, 1);
   render();
 }
 
-function checkRead(book, i) {
-  if (book.read) {
-    return `<button class="btn read" id='read-btn${i}' onclick='changeStatus(${i})'>Already read</button>`;
-  } else {
-    return `<button class="btn not-read" id='read-btn${i}' onclick='changeStatus(${i})'>You haven't read this book</button>`;
-  }
-}
-
-function render() {
-  let container = document.getElementById("book-cont");
-  container.innerHTML = "";
-  let testLibrary = JSON.parse(localStorage.getItem("testLibrary"));
-  testLibrary.forEach((book, i) => {
-    container.innerHTML += `<div class="book">
-                            <h1>${book.title}</h1>
-                            <h2>${book.author}</h2>
-                            <h2>${book.pages}</h2>
-                            <h2><span>Status:</span>${checkRead(book, i)}</h2>
-                            <button class="btn delete" id='delete-book${i}' onclick='removeBook(${i})'>Delete</button>
-                            </div>`;
-  });
-}
-
 function changeStatus(i) {
-  let button = document.getElementById("read-btn" + i);
+  const button = document.getElementById(`read-btn${i}`);
   if (button.innerHTML === "You haven't read this book") {
-    button.innerHTML = "Already read";
-    button.className = "btn read";
+    button.innerHTML = 'Already read';
+    button.className = 'btn read';
   } else {
     button.innerHTML = "You haven't read this book";
-    button.className = "btn not-read";
+    button.className = 'btn not-read';
   }
 }
 
 function displayForm() {
-  form.style.display = "block";
+  form.style.display = 'block';
 }
 
 function closeForm() {
-  form.style.display = "none";
+  form.style.display = 'none';
 }
+
+document.getElementById('new-book').addEventListener('click', displayForm);
+document.getElementById('close').addEventListener('click', closeForm);
+document.getElementById('add').addEventListener('click', addBookToLibrary);
