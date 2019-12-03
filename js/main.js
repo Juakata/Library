1f76ee5336fa458acb5c142f21c2fb698f674614
Juakata <ha.uzquiano@hotmail.com>
2019-12-03 10:44:22 -0600

    Create structure, styling for title, form and form functions

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let form = document.getElementById('form');
+
+let myLibrary = []
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+function render() {
+
+}
+
+function displayForm() {
+  form.style.display = "block";
+}
+
+function closeForm() {
+  form.style.display = "none";
+}
